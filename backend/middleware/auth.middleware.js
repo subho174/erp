c1e4320b535edee78c4f4d46147df75b448704ee
Subhodip <subhodipnebu51@gmail.com>
2025-03-27 23:43:29 +0530

    fixing issues

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -9,7 +9,7 @@ const verifyJWT = asynHandler(async (req, res, next) => {
     
     const token =
       req.cookies?.accessToken ||
-      req.header("Authorization")?.replace("Bearer", "");
+      req.header("Authorization")?.replace("Bearer ", "");
     if (!token) throw new ApiError(401, "Unauthorized request");
 
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
